0d3089d657b0633b8d3bdf79325668905b84ce64
imaliteralegend <mcytsinnit@gmail.com>
2025-12-05 02:35:36 +0100

    defi photo (en cours)

--- defis4.php
+++ defis4.php
@@ -1,6 +1,33 @@
 <?php
+$compteurjustes = 0; 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $justerecyclable = [1,2,5,7,8]; 
+    $justenonrecyclable = [3,4,6,9,10]; 
+
+    $reponse_oui = $_POST['oui'] ; 
+    $reponse_non = $_POST['non'] ;
+
+    
+    for ($i = 0; $i < count($justerecyclable); $i++) {
+    if (in_array($justerecyclable[$i], $reponse_oui)) {
+        $repjustes++;
+    }
+}
+
+
+    // Vérifier les réponses "non"
+    for ($i = 0; $i < count($justenonrecyclable); $i++) {
+    if (in_array($justenonrecyclable[$i], $reponse_oui)) {
+        $repjustes++;
+    }
+}
+
+
+    echo "<p>Vous avez $compteurjustes réponses correctes sur 10.</p>";
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang = "fr">
@@ -50,35 +77,35 @@
   <div>
     <figure>
      <img src="Images/imprimante.jpg" alt="Imprimante" width = 175>
-    <figcaption>Imprimante<br> n°3 </br></figcaption>
+    <figcaption>Imprimante<br> n°5 </br></figcaption>
 </figure>
   </div>
 
   <div>
     <figure>
      <img src="Images/leds.jpg" alt="Leds" width = 175>
-    <figcaption>LEDs<br> n°9 </br></figcaption>
+    <figcaption>LEDs<br> n°6</br></figcaption>
 </figure>
   </div>
 
   <div> 
   <figure>  
   <img src="Images/manette.jpg" alt="Manette" width = 175>
-    <figcaption>Manette <br> n°4 </br></figcaption>
+    <figcaption>Manette <br> n°7 </br></figcaption>
 </figure>
 </div>
 
   <div>
     <figure>
      <img src="Images/barretteram.jpg" alt="RAM" width = 175>
-    <figcaption>Barette de RAM<br> n°5 </br></figcaption>
+    <figcaption>Barette de RAM<br> n°8 </br></figcaption>
 </figure>
 </div>
 
 <div>
     <figure>
      <img src="Images/cables.jpg" alt="Cables" width = 175>
-    <figcaption>Cables (embouts brûlés) - n°6</figcaption>
+    <figcaption>Cables (embouts brûlés) - n°9</figcaption>
 </figure>
 </div>
 
@@ -86,83 +113,85 @@
 <div>
     <figure>
      <img src="Images/ecouteurs.jpg" alt="Ecouteurs filaires" width = 175>
-    <figcaption>Ecouteurs filaires<br> n°8 </br></figcaption>
+    <figcaption>Ecouteurs filaires<br> n°10 </br></figcaption>
 </figure>
   </div>
 
   
 
 </div>
-    
+   
+<form method = "POST">
 <div class="row">
   <div class="column">
     <h4> Recyclable </h4>
-<input type="checkbox" id="n1" name="n1" value="n1">
+<input type="checkbox" id="n1" name="oui[]" value="1">
 <label for="n1"> N°1 : Moniteur</label><br>
 
-<input type="checkbox" id="n2" name="n2" value="n2">
+<input type="checkbox" id="n2" name="oui[]" value="2">
 <label for="n2"> N°2 : Smartphone</label><br>
 
-<input type="checkbox" id="n3" name="n3" value="n3">
-<label for="n3"> N°3 : Imprimante</label><br>
+<input type="checkbox" id="n3" name="non[]" value="3">
+<label for="n3"> N°3 : Télécommande</label><br>
 
-<input type="checkbox" id="n4" name="n4" value="n4">
-<label for="n4"> N°4 : Manette</label><br>
+<input type="checkbox" id="n4" name="non[]" value="4">
+<label for="n4"> N°4 : Appareil photo jetable</label><br>
 
-<input type="checkbox" id="n5" name="n5" value="n5">
-<label for="n5"> N°5 : Barette de RAM</label><br>
+<input type="checkbox" id="n5" name="oui[]" value="5">
+<label for="n5"> N°5 : Imprimante</label><br>
 
-<input type="checkbox" id="n6" name="n6" value="n6">
-<label for="n6"> N°6 : Cables aux embouts brûlés</label><br>
+<input type="checkbox" id="n6" name="non[]" value="6">
+<label for="n6"> N°6 : LEDs</label><br>
 
-<input type="checkbox" id="n7" name="n7" value="n7">
-<label for="n7"> N°7 : Appareil photo jetable</label><br>
+<input type="checkbox" id="n7" name="oui[]" value="7">
+<label for="n7"> N°7 : Manette</label><br>
 
-<input type="checkbox" id="n8" name="n8" value="n8">
-<label for="n8"> N°8 : Ecouteurs filaires</label><br>
+<input type="checkbox" id="n8" name="oui[]" value="8">
+<label for="n8"> N°8 : Barette de RAM</label><br>
 
-<input type="checkbox" id="n9" name="n9" value="n9">
-<label for="n9"> N°9 : LEDs</label><br>
+<input type="checkbox" id="n9" name="non[]" value="9">
+<label for="n9"> N°9 : Cables aux embouts brûlés</label><br>
 
-<input type="checkbox" id="n10" name="n10" value="n10">
-<label for="n10"> N°10 : Télécommande</label><br>
+<input type="checkbox" id="n10" name="non[]" value="10">
+<label for="n10"> N°10 : Écouteurs filaires</label><br>
   </div>
   
   <div class="column">
        <h4> Non recyclable </h4>
-<input type="checkbox" id="n1" name="n1" value="n1">
-<label for="vehicle1"> N°1 : Moniteur</label><br>
+<input type="checkbox" id="n1-2" name="oui[]" value="1">
+<label for="n1-2"> N°1 : Moniteur</label><br>
 
-<input type="checkbox" id="n2" name="n2" value="n2">
-<label for="vehicle1"> N°2 : Smartphone</label><br>
+<input type="checkbox" id="n2-2" name="oui[]" value="2">
+<label for="n2-2"> N°2 : Smartphone</label><br>
 
-<input type="checkbox" id="n3" name="n3" value="n3">
-<label for="vehicle1"> N°3 : Imprimante</label><br>
+<input type="checkbox" id="n3-2" name="non[]" value="3">
+<label for="n3-2"> N°3 : Télécommande</label><br>
 
-<input type="checkbox" id="n4" name="n4" value="n4">
-<label for="vehicle1"> N°4 : Manette</label><br>
+<input type="checkbox" id="n4-2" name="non[]" value="4">
+<label for="n4-2"> N°4 : Appareil photo jetable</label><br>
 
-<input type="checkbox" id="n5" name="n5" value="n5">
-<label for="vehicle1"> N°5 : Barette de RAM</label><br>
+<input type="checkbox" id="n5-2" name="oui[]" value="5">
+<label for="n5-2"> N°5 : Imprimante</label><br>
 
-<input type="checkbox" id="n6" name="n6" value="n6">
-<label for="vehicle1"> N°6 : Cables aux embouts brûlés</label><br>
+<input type="checkbox" id="n6-2" name="non[]" value="6">
+<label for="n6-2"> N°6 : LEDs</label><br>
 
-<input type="checkbox" id="n7" name="n7" value="n7">
-<label for="vehicle1"> N°7 : Appareil photo jetable</label><br>
+<input type="checkbox" id="n7-2" name="oui[]" value="7">
+<label for="n7-2"> N°7 : Manette</label><br>
 
-<input type="checkbox" id="n8" name="n8" value="n8">
-<label for="vehicle1"> N°8 : Ecouteurs filaires</label><br>
+<input type="checkbox" id="n8-2" name="oui[]" value="8">
+<label for="n8-2"> N°8 : Barette de RAM</label><br>
 
-<input type="checkbox" id="n9" name="n9" value="n9">
-<label for="vehicle1"> N°9 : LEDs</label><br>
+<input type="checkbox" id="n9-2" name="non[]" value="9">
+<label for="n9-2"> N°9 : Cables aux embouts brûlés</label><br>
 
-<input type="checkbox" id="n10" name="n10" value="n10">
-<label for="vehicle1"> N°10 : Télécommande</label><br>
+<input type="checkbox" id="n10-2" name="non[]" value="10">
+<label for="n10-2"> N°10 : Écouteurs filaires</label><br>
   </div>
 </div>
 
-<button type="button">Valider ma réponse</button>
-
+<br>
+<button type="submit" class = "btn-action">Valider ma réponse</button>
+</form>
 
 </body>
